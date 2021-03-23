f436c8c578121d2bd259f881cb92c57b176859fd
abonnet <anthony.bonnet1@orange.fr>
2021-03-23 15:43:49 +0100

    Ajout table seeder

--- datamodel/seeder.js
+++ datamodel/seeder.js
@@ -6,6 +6,8 @@ module.exports = (itemService, listService) => {
             await listService.dao.db.query("CREATE TABLE list(id SERIAL PRIMARY KEY, shop TEXT NOT NULL, date DATE NOT NULL, archived BOOLEAN NOT NULL)")
             await itemService.dao.db.query("CREATE TABLE item(id SERIAL PRIMARY KEY, idList INTEGER NOT NULL, label TEXT NOT NULL, quantity INTEGER NOT NULL, checked BOOLEAN NOT NULL, FOREIGN KEY(idList) REFERENCES list(id))")
             // INSERTs
+
+
             resolve()
         } catch (e) {
             if (e.code === "42P07") { // TABLE ALREADY EXISTS https://www.postgresql.org/docs/8.2/errcodes-appendix.html

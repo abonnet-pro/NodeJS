87326d33f2ede609b37f95ca62c2a8104c8d7826
abonnet <anthony.bonnet1@orange.fr>
2021-06-09 11:16:49 +0200

    Correction bug reset password

--- services/useraccount.js
+++ services/useraccount.js
@@ -106,7 +106,7 @@ module.exports = class UserAccountService
             html: `<h1>Email de reinitialisation</h1>
                    <h2>Bonjour ${user.displayname}</h2>
                    <p>Afin de réinitialiser votre mot de passe veuillez cliquer sur le lien ci-dessous</p>
-                   <a href=http://ec2-54-164-198-238.compute-1.amazonaws.com:3333/reset.html?${user.reset}>Cliquez ici</a>
+                   <a href=http://ec2-54-164-198-238.compute-1.amazonaws.com/reset.html?${user.reset}>Cliquez ici</a>
                    <p>http://ec2-54-164-198-238.compute-1.amazonaws.com:3333/reset.html?${user.reset}</p>`
         }).catch(err => console.log(err))
     }
